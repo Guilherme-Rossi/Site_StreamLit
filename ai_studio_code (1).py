101d067db3ede70625c544d2ada7fe991e8cbb24
enrico_876 <enricosafonso36@gmail.com>
2025-11-01 21:25:50 -0300

    teste34242

--- ai_studio_code (1).py
+++ ai_studio_code (1).py
@@ -235,4 +235,12 @@ html_string = """
                             <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>Notificações por e-mail</li>
                             <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>Relatórios básicos</li>
                         </ul>
-                        <a href="#" class=
+                        <a href="#" class="btn btn-outline">Começar Teste</a>
+                    </div>
+                    <div class="pricing-card popular">
+                        <span class="popular-badge">Mais Popular</span>
+                        <h3>Premium</h3>
+                        <div class="price">R$150<span>/mês</span></div>
+                        <ul>
+                            <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>Até 50 usuários</li>
+                            <li><svg xmlns="http://www.
